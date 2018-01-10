4915af0ee2f04b4d52c84f7db22625ee9c879a73
jtorre39 <jtorre39@lion.lmu.edu>
2018-01-10 14:04:12 -0800

    Added javascript quicksort

---
+++ quicksort/quicksort.js
@@ -0,0 +1,25 @@
+// Simple quicksort implemented in JavaScript.
+// Space inefficient, can be improved.
+// Does not mutate the original array.
+
+const quicksort = (array) => {
+
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const arrayCopy = array.slice(0);
+    const pivot = arrayCopy.pop();
+    const lesser = [];
+    const greater = [];
+
+    arrayCopy.forEach((element) => {
+        if (element > pivot) {
+            greater.push(element);
+        } else {
+            lesser.push(element);
+        }
+    });
+
+ return quicksort(lesser).concat(pivot, quicksort(greater))
+};
