59a71bbd631bbd107c3594271dc07e1320c46a8e
jtorre39 <jtorre39@lion.lmu.edu>
2018-01-11 20:49:16 -0800

    Finalized elm quicksort

--- quicksort/quicksort.elm
+++ quicksort/quicksort.elm
@@ -1,18 +1,19 @@
 import Html exposing (text)
 import List exposing (partition, append)
 
+-- Right here let's us display the results in an HTML file
 main =
   text toString <| quicksort [-1, 100, -2, -4433, 10, 4, 19]
 
 -- Elm is a functional language, so we recursively quicksort without
 -- assignment or variables.
 
-quicksort: List number -> List number
+quicksort: List comparable -> List comparable
 quicksort list =
     case list of
-        [] -> list
+        [] -> []
         pivot :: rest ->
             let
-                (lesser, greater) = partition (\x -> x < (toInt pivot)) rest
+                (lesser, greater) = partition (\x -> x < pivot) rest
             in
-                (quicksort lesser) ++ pivot ++ (quicksort greater)
+                (quicksort lesser) ++ [pivot] ++ (quicksort greater)
