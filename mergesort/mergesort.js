185da6b3c1c4f14dfe7b1566a5a41dff5b11e966
jtorre39 <jtorre39@lion.lmu.edu>
2018-01-15 18:45:26 -0800

    saving code snippets

--- mergesort/mergesort.js
+++ mergesort/mergesort.js
@@ -1,12 +1,25 @@
 const mergeSort = (array) => {
+    if (array.length <= 1) {
+        return;
+    }
 
 
+
+    merge(mergeSort(array.sub), mergeSort(right))
 };
-this.x = 10;
 
-function f () {
-    this.
+
+const newArray = [];
+let a = 0;
+let b = 0;
+for (let i = 0; i < newArray.length; i++){
+    if (a !== left.length - 1 && left[a] < right[b]) {
+        newArray.push(left[a]);
+        a++;
+    } else if (b !== right.length - 1 && left[a] > right[b]) {
+        newArray.push(right[b]);
+        b++;
+    }
 }
 
-let a = {f};
-a.f();
+return newArray;
